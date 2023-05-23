c6e5c6f37b971ce547d0d933457635e231960ab5
NorrySkinnis <florian.handke@outlook.de>
2023-05-23 18:48:44 +0200

    Auto stash before merge of "Flo" and "main"

--- main.py
+++ main.py
@@ -12,8 +12,13 @@ from environment import Environment as env
 
 if __name__=='__main__':
     n_games = 10
+<<<<<<< Updated upstream
     n_matchups = 5
     n_generations = 100
+=======
+    n_matchups = 10
+    n_generations = 1
+>>>>>>> Stashed changes
 
     env = env(n_players=10, n_games=n_games, n_matchups=n_matchups)
     env.run(n_generations=n_generations, verbose=True)
