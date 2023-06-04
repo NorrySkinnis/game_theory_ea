27f5d9d23995ba7c42a85e7c7c50d19e3c965956
NorrySkinnis <florian.handke@outlook.de>
2023-06-04 09:59:15 +0200

    removed t4t player

--- src/player.py
+++ src/player.py
@@ -14,6 +14,7 @@ class Player:
 
         # Initialize brain, action history, reward, and number of matchups played
         self.brain = MLP(n_input=memory_capacity, n_hidden=4)
+        #self.brain = RMLP(n_input=memory_capacity, n_hidden_states=n_matchups, n_hidden=4)
         self.action_history = -np.ones(shape=(n_matchups, n_games+MAX_MEMORY_CAPACITY), dtype=int)
         self.reward = 0
         self.n_matchups_played = 0
@@ -49,61 +50,6 @@ class Player:
         self.n_matchups_played = 0
         self.initialize_action_history()
 
-    @classmethod
-    def t4tplayer(cls, identifier: int, n_matchups: int, n_games: int, memory_capacity=3):
-        """hardcode a t4t player with memory 3. args are the same as __init__()
-
-        Args:
-            identifier: unique identifier for player
-            n_matchups: number of matchups per generation
-            n_games: number of games per matchup
-            memory_capacity: (optional) number of previous actions to consider when making a decision
-        """
-        Player = cls(identifier, n_matchups, n_games, memory_capacity)
-        # capacity = 3
-        # Player.brain.W1_ = np.array([[-0.03709999634875267, -1.7961602824118976, -1.8527891849240585, -0.36712396749863263], [1.0231250035826696, -1.9575437119800085, 0.19572761446875336, 2.240618403104051], [3.1467075480031617, 2.268071929050388, -2.070629723532281, 1.2688080835096178]])
-        # Player.brain.W2_ = np.array([[4.053400565815565], [1.8971396987502256], [-0.7128677902049138], [-0.869760297130533]])
-        # Player.brain.Wb1 = np.array([[0.20788300846405733, -1.102598967259388, -0.4842573079389737, 0.9118712326838753]])
-        # Player.brain.Wb2 = np.array([[-3.9004539936852285]])
-
-        # capacity = 1
-        Player.brain.W1 = np.array([[0.39317879088646795, -1.1621958799752339, 1.5090907218007212, -0.578700719287135], [0.2663267808857011, -1.9243267343076034, -2.263988110778135, 3.2121356772178773]])
-        Player.brain.W2 = np.array([[-0.2582632261183214], [-0.6516885143700015], [-0.0703517480993997], [1.5090354704682072]])
-        Player.brain.Wb1 = np.array([[0.6608086435270056, 1.2096007035349565, 0.2686518690291412, 0.3545352896601341]])
-        Player.brain.Wb2 = np.array([[-0.8375013776060737]])
-        
-        return Player
-
-    # def crossover(self, other):
-    #     """Cross over genes of player with another
-
-    #     Args:
-    #         other (Player): player to cross over with
-    #     """
-    #     crossover_p = 0.1
-    #     # switch weight vectors randomly between 2 players
-
-    #     for i, col in enumerate(self.brain.W1[0]):
-    #         if random.random() < crossover_p:
-    #             temp = self.brain.W1[:,i]
-    #             self.brain.W1[:,i] = other.brain.W1[:,i]
-    #             other.brain.W1[:,i] = temp
-    #     for i, col in enumerate(self.brain.W2[0]):
-    #         if random.random() < crossover_p:
-    #             temp = self.brain.W2[:,i]
-    #             self.brain.W2[:,i] = other.brain.W2[:,i]
-    #             other.brain.W2[:,i] = temp
-    #     for i, col in enumerate(self.brain.Wb1[0]):
-    #         if random.random() < crossover_p:
-    #             temp = self.brain.Wb1[:,i]
-    #             self.brain.Wb1[:,i] = other.brain.Wb1[:,i]
-    #             other.brain.Wb1[:,i] = temp
-    #     for i, col in enumerate(self.brain.Wb2[0]):
-    #         if random.random() < crossover_p:
-    #             temp = self.brain.Wb2[:,i]
-    #             self.brain.Wb2[:,i] = other.brain.Wb2[:,i]
-    #             other.brain.Wb2[:,i] = temp
-    
 class MLP():
     """Creates a multi-layer perceptron with a single hidden layer."""
     def __init__(self, n_input, n_hidden):
@@ -145,56 +91,88 @@ class MLP():
         self.W2 += np.random.normal(loc, scale, size=self.W2.shape)
         self.Wb1 += np.random.normal(loc, scale, size=self.Wb1.shape)
         self.Wb2 += np.random.normal(loc, scale, size=self.Wb2.shape)
+
+    def crossover(self, other):
+        """Cross over genes of player with another
+
+        Args:
+            other (Player): player to cross over with
+        """
+        crossover_p = 0.1
+        # switch weight vectors randomly between 2 players
+
+        for i, col in enumerate(self.brain.W1[0]):
+            if random.random() < crossover_p:
+                temp = self.brain.W1[:,i]
+                self.brain.W1[:,i] = other.brain.W1[:,i]
+                other.brain.W1[:,i] = temp
+        for i, col in enumerate(self.brain.W2[0]):
+            if random.random() < crossover_p:
+                temp = self.brain.W2[:,i]
+                self.brain.W2[:,i] = other.brain.W2[:,i]
+                other.brain.W2[:,i] = temp
+        for i, col in enumerate(self.brain.Wb1[0]):
+            if random.random() < crossover_p:
+                temp = self.brain.Wb1[:,i]
+                self.brain.Wb1[:,i] = other.brain.Wb1[:,i]
+                other.brain.Wb1[:,i] = temp
+        for i, col in enumerate(self.brain.Wb2[0]):
+            if random.random() < crossover_p:
+                temp = self.brain.Wb2[:,i]
+                self.brain.Wb2[:,i] = other.brain.Wb2[:,i]
+                other.brain.Wb2[:,i] = temp
     
-    class RMLP():
-        """Creates a recurrent multi-layer perceptron with a single hidden layer."""
-        def __init__(self, n_input, n_hidden):
-            # Weights of first fully connected layer
-            self.W1 = np.random.normal(loc=0, scale=2, size=(n_input, n_hidden))
-            # Weights of second fully connected layer
-            self.W2 = np.random.normal(loc=0, scale=2, size=(n_hidden, 1))
-            # Weights of hidden layer
-            self.Wh = np.random.normal(loc=0, scale=2, size=(n_hidden, n_hidden))
-            # Bias of first fully connected layer
-            self.Wb1 = np.random.normal(loc=0, scale=2, size=(1, n_hidden))
-            # Bias of second fully connected layer
-            self.Wb2 = np.random.normal(loc=0, scale=2, size=(1, 1))
-            # Hidden state 
-            self.h = np.random.normal(loc=0, scale=2, size=(n_hidden, 2))
-            # Activation function (ReLU)
-            self.f1 = lambda x: np.maximum(0, x)  
+class RMLP():
+    """Creates a recurrent multi-layer perceptron with a single hidden layer."""
+    def __init__(self, n_input, n_hidden_states, n_hidden):
+        # Weights of first fully connected layer
+        self.W1 = np.random.normal(loc=0, scale=1, size=(n_input, n_hidden))
+        # Weights of second fully connected layer
+        self.W2 = np.random.normal(loc=0, scale=1, size=(n_hidden, 1))
+        # Weights of hidden layer
+        self.Wh = np.random.normal(loc=0, scale=1, size=(n_hidden, n_hidden))
+        # Bias of first fully connected layer
+        self.Wb1 = np.random.normal(loc=0, scale=1, size=(1, n_hidden))
+        # Bias of second fully connected layer
+        self.Wb2 = np.random.normal(loc=0, scale=1, size=(1, 1))
+        # Hidden state 
+        self.h = np.random.normal(loc=0, scale=1, size=(n_hidden_states, n_hidden))
+        # Activation function (ReLU)
+        self.f1 = lambda x: np.maximum(0, x)  
 
-        def forward(self, X: np.ndarray) -> np.ndarray:
-            """ Forwards input through network and returns actions.
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        """ Forwards input through network and returns actions.
 
-            Parameters:
-            -----------
-            X: (np.ndarray)
-                Input matrix of shape (n, m), where n is the number of opponents 
-                and m is the number of actions observed.
-            
-            Returns:
-            --------
-            output: (np.ndarray)
-                Output matrix of shape (n, 1), one action for each opponent. 
-            """
-            # Compute hiddent state using previous hidden state
-            self.h[:,1] = self.f1(X @ self.W1 + self.h[:,0] @ self.Wh + self.Wb1)
-            output = self.h[:,1] @ self.W2 + self.Wb2
-            output = np.array(output >= 0, dtype=bool) * 1
-            output = np.reshape(output, (output.shape[0],))
-            # Update hidden state
-            self.h[:,0] = self.h[:,1]
-            return output
+        Parameters:
+        -----------
+        X: (np.ndarray)
+            Input matrix of shape (n, m), where n is the number of opponents 
+            and m is the number of actions observed.
         
-        def mutate(self):
-            """ Mutate neuron connections of brain."""
-            loc = 0
-            scale = 1
-            self.W1 += np.random.normal(loc, scale, size=self.W1.shape)
-            self.W2 += np.random.normal(loc, scale, size=self.W2.shape)
-            self.Wh += np.random.normal(loc, scale, size=self.Wh.shape)
-            self.Wb1 += np.random.normal(loc, scale, size=self.Wb1.shape)
-            self.Wb2 += np.random.normal(loc, scale, size=self.Wb2.shape)
+        Returns:
+        --------
+        output: (np.ndarray)
+            Output matrix of shape (n, 1), one action for each opponent. 
+        """
+        n_hidden_states = self.X.shape[0]
+        # Compute hiddent state using previous hidden state
+        #print(self.f1(X @ self.W1 + self.h[0,:] @ self.Wh + self.Wb1))
+        self.h[1,:] = self.f1(X @ self.W1 + self.h[0,:] @ self.Wh + self.Wb1)
+        output = self.h[1,:] @ self.W2 + self.Wb2
+        output = np.array(output >= 0, dtype=bool) * 1
+        output = np.reshape(output, (output.shape[0],))
+        # Update hidden state
+        self.h[0,:] = self.h[1,:]
+        return output
+    
+    def mutate(self):
+        """ Mutate neuron connections of brain."""
+        loc = 0
+        scale = 1
+        self.W1 += np.random.normal(loc, scale, size=self.W1.shape)
+        self.W2 += np.random.normal(loc, scale, size=self.W2.shape)
+        self.Wh += np.random.normal(loc, scale, size=self.Wh.shape)
+        self.Wb1 += np.random.normal(loc, scale, size=self.Wb1.shape)
+        self.Wb2 += np.random.normal(loc, scale, size=self.Wb2.shape)
 
           
