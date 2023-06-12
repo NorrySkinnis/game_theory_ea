b7413b2f3412ad860a97fc5c2c94383cc48a9874
S.J. Boosman <stijn.boosman@pm.me>
2023-06-12 10:34:02 +0200

    permute and rounding

--- main.py
+++ main.py
@@ -26,11 +26,10 @@ def run(n_games=50, n_matchups=70, n_generations=100, n_players=100, elite=0.5, 
 
 def permute_rates(step_size_mutation=0.1, step_size_elite=0.1):
     mutations = np.arange(0, 1, step_size_mutation)
-    # elite_rates = np.arange(step_size_elite, 0.8, step_size_elite)
-    elite = 0.4
-    for mut_r in mutations:
+    elite_rates = np.arange(step_size_elite, 0.8, step_size_elite)
+    for mut_r, elite_r in product(mutations, elite_rates):
         # smaller parameters to decrease run time
-        run(n_players=50, n_games=30, n_matchups=30, elite=elite, mutation_rate=round(mut_r, 2), n_generations=50,
+        run(n_players=50, n_games=30, n_matchups=30, elite=round(elite_r, 2), mutation_rate=round(mut_r, 2), n_generations=50,
             figure_path="./src/figures/permute_rates")
 
 
@@ -49,6 +48,6 @@ if __name__ == '__main__':
     assert n_players > 1 and n_players % 2 == 0, 'n_players must be even and larger than 1'
     assert memory_capacity <= MAX_MEMORY_CAPACITY and memory_capacity > 0, f'memory_capacity must be between 1 and {MAX_MEMORY_CAPACITY}'
     
-    run(n_games=n_games, n_matchups=n_matchups, n_generations=n_generations, n_players=n_players, elite=elite,
-          memory_capacity=memory_capacity, mutation_rate=mutation_rate, verbose=False)
-    # permute_rates(step_size_mutation=0.2, step_size_elite=0.2)
+    # run(n_games=n_games, n_matchups=n_matchups, n_generations=n_generations, n_players=n_players, elite=elite,
+    #       memory_capacity=memory_capacity, mutation_rate=mutation_rate, verbose=False)
+    permute_rates(step_size_mutation=0.2, step_size_elite=0.2)
