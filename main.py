0f65a6e544196ac420b94394c70b65778283d447
NorrySkinnis <florian.handke@outlook.de>
2023-06-18 23:53:49 +0200

    ready for simulations

--- main.py
+++ main.py
@@ -17,39 +17,50 @@ from evaluation import Evaluator
 
 if __name__ == '__main__':
 
+    # Set payoff matrix and fitness function
+    payoff_matrix = np.array([[(3, 3), (0, 5)], [(5, 0), (1, 1)]])
+    fitness = lambda x, t: np.power(1, t) * np.sum(x)
+
     # Set simulation parameters
-    n_players = [50] # must be even
-    n_matchups = [30]
-    n_games = [20]
-    n_generations = [100]
+    n_players = 50 # must be even
+    n_generations = 100
+    n_matchups = 20
+    n_games = 30
 
     # Set simulation hyperparameters
-    memory_capacity = [3, 2, 3]
-    elite = [0.5, 0.5, 0.95]
-    mutation_rate = [0.6, 0.5, 1]
-    crossover = [True]
-    crossover_p= [0.2, 0.3, 0.6]
+    memory_capacity = [1, 2, 3]
+    elite = [0.1, 0.5, 0.95]
+    mutation_rate = [0.1, 0.5, 1]
+    crossover_rate = [0, 0.3, 0.6]
 
     # Create permutations of settings
-    sim_settings = itertools.product(n_players, n_matchups, n_games, n_generations, 
-                                     memory_capacity, elite, mutation_rate, crossover, 
-                                     crossover_p)
+    sim_settings = itertools.product([n_players], [n_generations], [n_matchups], [n_games],
+                                     memory_capacity, elite, mutation_rate, crossover_rate)
     
-    repetitions = 5
+    # Iterate over simulation settings and run simulations n times
+    n_simulations = 10
     for setting in sim_settings:
-        s = []
-        for _ in range(repetitions):
-            simulation = Environment(*setting)
+
+        evaluator = Evaluator(*setting, n_simulations=n_simulations, payoff_matrix=payoff_matrix)
+        
+        for _ in range(n_simulations):
+
+            simulation = Environment(*setting, payoff_matrix=payoff_matrix, fitness=fitness)
+            simulation.attach(evaluator)
             simulation.run(verbose=False)
-            strategy_data = simulation.evaluator.strategy_data
-            s.append(strategy_data)
-        Evaluator.plot_average_strategies(s)
+            # # Get codes of undetermined strategies
+            # unknown = simulation.detector.undetermined_strategies
+            # strategy_count = Counter(frozenset(s) for s in unknown)
+            # print(strategy_count)
+            evaluator.plot_fitness()
+            evaluator.plot_strategies()
+
+        evaluator.plot_average_fitness()
+        evaluator.plot_average_strategies()
+
         break
         
 
     # Used printing out the codes of the undetermined strategies
 
-    # unknown = env.detector.undetermined_strategies
-    # strategy_count = Counter(frozenset(s) for s in unknown)
-    # print(strategy_count)
 
